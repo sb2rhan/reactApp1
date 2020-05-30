079a9b25bd3349ced525e346e06775ed277a4fe7
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-05-30 21:18:36 +0600

    my_files to my_components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 // import _variableOfFunction_ from _pathToFunction_
-import Header from './my_files/Header';
-import Technologies from './my_files/Technologies'
-import Footer from './my_files/Footer'
+import Header from './my_components/Header';
+import Technologies from './my_components/Technologies'
+import Footer from './my_components/Footer'
 
 const App = () => {
   // This is not HTML, this is JSX
--- src/my_components/Header.js
+++ src/my_components/Header.js
@@ -4,12 +4,7 @@ const Header = () => {
     // This is not HTML, this is JSX
   return (
     <div className="Header">
-      <ul>
-        <a href="/">Home</a> -
-        <a href="/">News</a> -
-        <a href="/">Feed</a> -
-        <a href="/">Messages</a>
-      </ul>
+      
     </div>
   );
 }
