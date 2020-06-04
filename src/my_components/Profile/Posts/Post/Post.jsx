86d742cbf368fb30a6776e82d042bbc5f260ee85
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-06-04 21:02:37 +0600

    i added props to post

--- src/my_components/Profile/Posts/Post/Post.jsx
+++ src/my_components/Profile/Posts/Post/Post.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.post}>
             <img alt="avatar" src="https://www.kindpng.com/picc/m/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi suscipit adipisci, illum deleniti natus omnis optio voluptate animi modi quis ipsum aliquam, ratione, officiis qui exercitationem eligendi assumenda eaque hic.
-            </p>
+            <p>{props.message}</p>
             <div>
-                <span>Like</span>
+                <span>{props.likes} likes</span>
             </div>
         </div>
     );
--- src/my_components/Profile/Posts/Posts.jsx
+++ src/my_components/Profile/Posts/Posts.jsx
@@ -6,10 +6,10 @@ const Posts = () => {
     return (
         <div className={s.posts}>
             <h2>My Posts</h2>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post message="Hi, how are you guys?" likes="23"/>
+            <Post message="The guy who wrote this is my brother" likes="34"/>
+            <Post message="Hello, World!" likes="2"/>
+            <Post message="Welcome to your first post." likes="10"/>
         </div>
     );
 }
