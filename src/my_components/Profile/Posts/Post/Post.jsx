f88714eca2d3f164ddd6d2d21c1528840d467273
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-06-04 16:18:24 +0600

    like button

--- src/my_components/Profile/Posts/Post/Post.jsx
+++ src/my_components/Profile/Posts/Post/Post.jsx
@@ -5,6 +5,12 @@ const Post = () => {
     return (
         <div className={s.post}>
             <img alt="avatar" src="https://www.kindpng.com/picc/m/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"/>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi suscipit adipisci, illum deleniti natus omnis optio voluptate animi modi quis ipsum aliquam, ratione, officiis qui exercitationem eligendi assumenda eaque hic.
+            </p>
+            <div>
+                <span>Like</span>
+            </div>
         </div>
     );
 }
