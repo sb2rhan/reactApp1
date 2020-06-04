9d12f66b3a66d02771f062e20aedcf688199f3e3
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-06-04 14:52:35 +0600

    file seperation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 // import _variableOfFunction_ from _pathToFunction_
-import Header from './my_components/Header';
-import Navbar from './my_components/Navbar';
-import Profile from './my_components/Profile';
-import Footer from './my_components/Footer';
+import Header from './my_components/Header/Header';
+import Navbar from './my_components/Navbar/Navbar';
+import Profile from './my_components/Profile/Profile';
+import Footer from './my_components/Footer/Footer';
 
 const App = () => {
   // This is not HTML, this is JSX
