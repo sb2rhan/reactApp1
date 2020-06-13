3ab28b2675e68981a60a927f36b76ce09639da67
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-06-13 20:25:19 +0600

    optimized links in navbar

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import Header from './my_components/Header/Header';
 import Navbar from './my_components/Navbar/Navbar';
 import Profile from './my_components/Profile/Profile';
 import Footer from './my_components/Footer/Footer';
-import Dialogs from './my_components/Dialogs/Dialogs'
+import Dialogs from './my_components/Dialogs/Dialogs';
+import News from './my_components/News/News';
+import Music from './my_components/Music/Music';
+import Settings from './my_components/Settings/Settings'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
   // This is not HTML, this is JSX
   return (
-    <div className="App">
-      <Header/>
-      <Navbar/>
-      <div className="content">
-        {/* <Profile/> */}
-        <Dialogs/>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Navbar />
+        <div className="content">
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/my_components/Navbar/Navbar.jsx
+++ src/my_components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import s from './Navbar.module.css'
+import s from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className={s.navbar}>
-            <div className={`${s.item} ${s.active}`}>
-                <a href="#">Profile</a>
+            <div className={s.item}>
+                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href="#">Messages</a>
+                <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href="#">News</a>
+                <NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="#">Music</a>
+                <NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href="#">Settings</a>
+                <NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink>
             </div>
         </nav>
     );
